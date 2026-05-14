7904b667bde44e471b30416a8f1ec292b4fe5068
Paul5123 <paul.guillon17@orange.fr>
2026-05-14 15:08:07 +0200

    Ajouts visuels des filtres et changements sur la structure du fichier JS

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -2,9 +2,12 @@
 /*Recuperation des données de l'api*/
 async function recupererTraveaux() {
     const reponse = await fetch("http://localhost:5678/api/works");
-    const traveaux = await reponse.json();
+    return await reponse.json();
+}
 
-    console.log(traveaux);
+async function implementerTraveaux() {
+
+    const traveaux = await recupererTraveaux();
 
     /* Variable gallery qui s'associe a la class "gallery" de l'html */
     let gallery = document.querySelector(".gallery")
@@ -21,8 +24,59 @@ async function recupererTraveaux() {
         gallery.innerHTML += html
     })
 }
+/* appel de la fonction qui recupere et  affiche les galleries*/
+implementerTraveaux();
 
-recupererTraveaux();
+async function recupererCategories() {
+    const reponse = await fetch("http://localhost:5678/api/categories");
+    return await reponse.json();
+}
+
+async function implementerCategories() {
+
+    const categories = await recupererCategories();
+    let portfolio = document.getElementById("portfolio");
+    let gallery = document.querySelector(".gallery");
+    let div = document .createElement("div");
+    div.classList.add("divfiltre-bouton");
+   
+    portfolio.insertBefore(div, gallery);
+
+
+        let html = `
+            <button type="button" class="filtre-bouton Tous">
+		        <span> Tous </span>
+	        </button>
+        `
+        div.innerHTML += html
+
+        /* Ouverture de la boucle qui me permet de creer les differents boutton */
+    categories.forEach(function(objet) {
+    /*Ajout des gallery dans le html */
+        let html = `
+            <button type="button" class="filtre-bouton ${objet.name}">
+		        <span> ${objet.name}</span>
+	        </button>
+        `
+        div.innerHTML += html
+    })
+}
+
+/* appel de la fonction qui recupere et  affiche les filtres */
+implementerCategories()
+
+
+/* 
+const boutonFiltrerObjets = document.querySelector(".Objets");
+
+boutonFiltrerObjets.addEventListener("click", function () {
+    const objetFiltrees = categories.filter(function (categories) {
+        return objet.category.name = "Objets";
+    })
+})
+
+*/
+
 
 
 
