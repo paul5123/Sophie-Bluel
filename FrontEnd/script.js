b4a28db2aefd576eb649eb05ae75322d9456559f
Paul5123 <paul.guillon17@orange.fr>
2026-05-15 17:50:00 +0200

    Ajout des fonctionnalités de la page log in.

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -13,7 +13,7 @@ async function recupererCategories() {
 
 /*Fonction qui creer la galleries dans le html*/
 function html(travaux) {
-    let gallery = document.querySelector(".gallery")
+  let gallery = document.querySelector(".gallery");
   travaux.forEach(function (objet) {
     let html = `
             <figure class="projet">
@@ -25,16 +25,12 @@ function html(travaux) {
   });
 }
 
-
 /***Fonction de  l'affichage de la gallerie par default ***/
 async function implementerTravaux() {
   const travaux = await recupererTravaux();
-  /* Appel de la fonction qui me permet de creer les differentes gallery */
- html(travaux,);
+  html(travaux);
 }
 implementerTravaux();
-
-
 
 /*Fonction de l'affichage des bouttons filtres "categories" */
 async function implementerCategories() {
@@ -64,27 +60,67 @@ async function implementerCategories() {
 implementerCategories();
 
 async function filtreBoutton() {
-    const travaux = await recupererTravaux();
-    const bouton = document.querySelectorAll(".filtre-bouton");
-    
-    bouton.forEach(function (bouton) {
-        bouton.addEventListener("click", function() {
-          const idBouton = Number(bouton.dataset.categoryId);
-          if(idBouton>0){ 
-            const Filtre = travaux.filter(function (objet) {
-              return objet.categoryId === idBouton;
-            })
-          console.log(Filtre)
-          document.querySelector(".gallery").innerHTML = "";
-          html(Filtre)
-          } else {
-           console.log(travaux)
-          document.querySelector(".gallery").innerHTML = "";
-          html(travaux)
-          }
-        })
-    
-})
-}
+  const travaux = await recupererTravaux();
+  const bouton = document.querySelectorAll(".filtre-bouton");
 
-filtreBoutton()
+  bouton.forEach(function (bouton) {
+    bouton.addEventListener("click", function () {
+      const idBouton = Number(bouton.dataset.categoryId);
+      if (idBouton > 0) {
+        const Filtre = travaux.filter(function (objet) {
+          return objet.categoryId === idBouton;
+        });
+        console.log(Filtre);
+        document.querySelector(".gallery").innerHTML = "";
+        html(Filtre);
+      } else {
+        console.log(travaux);
+        document.querySelector(".gallery").innerHTML = "";
+        html(travaux);
+      }
+    });
+  });
+}
+filtreBoutton();
+
+
+function connexion () {
+  const seConnecter = document.querySelector("#login form");
+
+  seConnecter.addEventListener("submit", async function (event){
+    event.preventDefault();
+
+    const identifiant = {
+      email: event.target.querySelector("#email").value,
+      password: event.target.querySelector("#password").value,
+    };
+
+    const chargeUtile = JSON.stringify(identifiant);
+
+    const reponse = await fetch("http://localhost:5678/api/users/login",{
+      method:"POST",
+      headers: {"Content-Type": "application/json"},
+      body: chargeUtile
+      });
+
+    const data = await reponse.json();
+    if (reponse.ok) {
+    localStorage.setItem("token", data.token);
+    window.location.href = "index.html";
+    } else {
+    const erreurP = document.createElement("p");
+    erreurP.style.color = "red";
+    const login = document.getElementById("login")
+    const aLogin = document.querySelector("#login a")
+    login.insertBefore(erreurP, aLogin);
+    erreurP.innerText = ("Les informations utilisateur / mot de passe ne sont pas correctes.  ")
+
+    }
+
+      console.log(identifiant)
+      console.log(data);
+
+
+  }); 
+}
+connexion ()
