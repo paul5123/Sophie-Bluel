26ff1b1b3ac68359cc1f628561fc11c26b071d48
Paul5123 <paul.guillon17@orange.fr>
2026-05-21 18:19:04 +0200

    Ajout du modale

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,19 +1,18 @@
-
-/*** Fonction creer pour le reste du code***/
-/*Fonction qui recupere les données de "works"*/
+/** Recuperation des données de l'api **/
+/*Recuperation des données de "works"*/
 async function recupererTravaux() {
   const reponse = await fetch("http://localhost:5678/api/works");
   return await reponse.json();
 }
 
-/*Fonction qui recupere les données de "categories"*/
+/*Recuperation des données de "categories"*/
 async function recupererCategories() {
   const reponse = await fetch("http://localhost:5678/api/categories");
   return await reponse.json();
 }
 
-/*Fonction qui creer la galleries dans le html*/
-function html(travaux) {
+/* Creation du shemas html de la gallerie pour la page index */
+function afficherGalerie(travaux) {
   let gallery = document.querySelector(".gallery");
   travaux.forEach(function (objet) {
     let html = `
@@ -26,13 +25,14 @@ function html(travaux) {
   });
 }
 
-/***Fonction de  l'affichage de la gallerie par default ***/
+/** Affichage par default du site de la page index **/
+/*Affichage de la gallerie par default */
 async function affichageTravaux() {
   const travaux = await recupererTravaux();
-  html(travaux);
+  afficherGalerie(travaux);
 }
 
-/*Fonction de l'affichage des boutons filtres "categories" */
+/*Affichage des boutons filtres "categories" */
 async function affichageCategories() {
   const categories = await recupererCategories();
   let portfolio = document.getElementById("portfolio");
@@ -40,6 +40,7 @@ async function affichageCategories() {
   let div = document.createElement("div");
   div.classList.add("divfiltre-bouton");
   portfolio.insertBefore(div, gallery);
+
   /* Ajout en dehors de la boucle, du bouton "tous" */
   let html = `
             <button type="button" class="filtre-bouton" data-category-id="0">
@@ -59,6 +60,8 @@ async function affichageCategories() {
   });
 }
 
+/** Affichage dynamique de la page index selon l'utilisation **/
+/* Filtre la gallerie au clique d'un des filtres */
 async function filtreBouton() {
   const travaux = await recupererTravaux();
   const bouton = document.querySelectorAll(".filtre-bouton");
@@ -71,45 +74,119 @@ async function filtreBouton() {
           return objet.categoryId === idBouton;
         });
         document.querySelector(".gallery").innerHTML = "";
-        html(Filtre);
+        afficherGalerie(Filtre);
       } else {
         document.querySelector(".gallery").innerHTML = "";
-        html(travaux);
+        afficherGalerie(travaux);
       }
     });
   });
 }
 
-function affichageModeEdition () {
-const classEdition = document.querySelectorAll(".edition");
-classEdition.forEach(function(html) {
-  html.style.display = "flex";
-});
-const filtresEditionTitre = document.querySelector("#filtres-Edition h2");
-filtresEditionTitre.style.marginBottom = 0;
-const filtreEdition = document.querySelector("#filtres-Edition");
-filtreEdition.style.marginBottom = "128px";
-const loginAffichage = document.querySelector("#edition-login a");
-loginAffichage.innerText = "logout";
-loginAffichage.href = "#";
-loginAffichage.addEventListener("click", function(){
-  localStorage.removeItem("token");
-  window.location.href = "index.html";
-});
+/* Affichage du mode edition lorsque l'on est connecté */
+function affichageModeEdition() {
+  const classEdition = document.querySelectorAll(".edition");
+  classEdition.forEach(function (html) {
+    html.style.display = "flex";
+  });
+  const filtresEditionTitre = document.querySelector("#filtres-Edition h2");
+  filtresEditionTitre.style.marginBottom = 0;
+  const filtreEdition = document.querySelector("#filtres-Edition");
+  filtreEdition.style.marginBottom = "128px";
+  const loginAffichage = document.querySelector("#edition-login a");
+  loginAffichage.innerText = "logout";
+  loginAffichage.href = "#";
+  loginAffichage.addEventListener("click", function () {
+    localStorage.removeItem("token");
+    window.location.href = "index.html";
+  });
 }
 
+/** Fonctionnement et affichage  du modale **/
+/* Affichage et modfification du modale au clique */
+function affihageModale() {
+  const boutonModifier = document.querySelectorAll(".edition-bouton");
+  const modale = document.querySelector(".modal");
+  const croix = document.querySelector(".modal-croix");
+  const boutonAjoutPhoto = document.querySelector(".changement-modal1");
+  const boutonValider = document.querySelector(".changement-modal2");
+  const modale1 = document.querySelector(".modal1");
+  const modale2 = document.querySelector(".modal2");
+  const flecheRetour = document.querySelector(".modal-retour");
+/* Fonction qui affiche la modale "gallerie" et cache "Ajout photo"*/
+  function afficherGalerie() {
+    modale1.style.display = "block";
+    modale2.style.display = "none";
+    boutonAjoutPhoto.style.display = "block";
+    boutonValider.style.display = "none";
+}
+/* Fonction qui affiche la modale "Ajout photo" et cache "gallerie"*/
+function afficherAjoutPhoto() {
+    modale1.style.display = "none";
+    modale2.style.display = "block";
+    boutonAjoutPhoto.style.display = "none";
+    boutonValider.style.display = "block";
+    boutonValider
+}
+  /* Affichage du moodal au clique de "modifier" */
+  boutonModifier.forEach(function (bouton) {
+    bouton.addEventListener("click", function () {
+      modale.style.display = "flex";
+      afficherGalerie();
+      modale.setAttribute("aria-hidden", "false");
+    });
+  });
+  /*Fermeture du modale au clique sur la croix ou a l'exterieur du modale */
+  function fermerModale() {
+    document.activeElement.blur();
+    modale.style.display = "none";
+    modale.setAttribute("aria-hidden", "true");
+  }
+  croix.addEventListener("click", fermerModale);
+  modale.addEventListener("click", function (event) {
+    if (event.target === modale) {
+      fermerModale();
+    }
+  });
+  /* Passage au modal2 au clique du bouton "Ajouter une photo" */
+  boutonAjoutPhoto.addEventListener("click", afficherAjoutPhoto);
+  /* Retour au modal 1 au clique de la feche */
+  flecheRetour.addEventListener("click", afficherGalerie);
+}
 
+/* Creation du shemas html de la gallerie pour la page du modale */
+function afficherGalerieModale(travaux) {
+  let modalGallery = document.querySelector(".modal-gallery");
+  travaux.forEach(function (objet) {
+    let html = `
+            <figure class="projet">
+		        <img src="${objet.imageUrl}" alt="${objet.title}">
+            <button class="modal-poubelle">
+        		<i class="fa-solid fa-trash-can"></i>
+   				</button>	
+	        </figure>
+        `;
+    modalGallery.innerHTML += html;
+  });
+}
 
+/* Affichage de la gallerie dans le modale */
+async function affichageTravauxModale() {
+  const travaux = await recupererTravaux();
+  afficherGalerieModale(travaux);
+}
 
+/*** Fonction qui appel toute les fonction du fichiers en async ***/
 async function fonctionement() {
   await affichageTravaux();
   const token = localStorage.getItem("token");
-  console.log("token =", token);
   if (token) {
     affichageModeEdition();
-  } else { 
-  await affichageCategories();
-  await filtreBouton();
+    affihageModale();
+    affichageTravauxModale();
+  } else {
+    await affichageCategories();
+    await filtreBouton();
   }
 }
 
